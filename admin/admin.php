16d920c47fb917d6b3a5ff34a4310cdfc05e31f6
nguyen an <nguyen.an263@gmail.com>
2014-03-14 10:21:08 +0700

    thay doi admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -2,6 +2,7 @@
 <?php include"../include/siderbar-admin.php" ?>
  <div id="content">
    <h1>Wellcome to cms admin</h1>
+   <h3>chao mung ban den voi trang web dau tien cua toi :D</h3>
  </div><!--end content-->       
 
 
